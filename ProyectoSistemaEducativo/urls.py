04e2ffa6cfede99eff75f78ca2a9503d76893ed5
Draaft4 <draaft@outlook.es>
2022-02-17 07:13:19 -0500

    Primer commir

---
+++ ProyectoSistemaEducativo/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from appSistemaEducativo.View import views
+
+urlpatterns = [
+    path('', views.Controlador.index),
+    path('login/',views.Controlador.login),
+    path('index/',views.Controlador.indexLg),
+    path('onlogin/',views.Controlador.onLogin),
+    path('index/profile/',views.Controlador.profile),
+    path('buscarDocente/',views.Controlador.buscarDocente),
+    path('ofertaAcademica/',views.Controlador.ofertaAcademica),
+    path('search/',views.Controlador.search),
+    path('profileDocente/',views.Controlador.profileDocente),
+    path('matriculacion/',views.Controlador.getMatriculacion),
+    path('matriculacion/agregar/',views.Controlador.matriculacionAgregar),
+    path('logout/',views.Controlador.logout),
+    path('',include('pwa.urls'))
+]
